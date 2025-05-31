cf5592cf801ac7012f099e89a1af0cfff7e52c08
scoughlan <sean.coughlan@utmost.co>
2025-05-31 20:23:06 +0100

    no web server

--- album-art-fetcher/src/main/kotlin/com/excentric/MusicBrainzApplication.kt
+++ album-art-fetcher/src/main/kotlin/com/excentric/MusicBrainzApplication.kt
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
 import org.springframework.web.client.RestTemplate
 import java.time.Duration.ofSeconds
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     SpringApplication.run(MusicBrainzApplication::class.java, *args)
@@ -21,12 +24,20 @@ open class MusicBrainzApplication {
 
     @Bean
     open fun restTemplate(): RestTemplate {
-
         return RestTemplateBuilder()
             .setConnectTimeout(FIVE_SECONDS)
             .setReadTimeout(FIVE_SECONDS)
             .build().apply {
                 messageConverters.add(0, MappingJackson2HttpMessageConverter())
             }
+    }
+}
+
+@ShellComponent
+class ApplicationCommands {
+
+    @ShellMethod(key = ["q"], value = "Exit the application immediately")
+    fun quit() {
+        exitProcess(0)
     }
 }
