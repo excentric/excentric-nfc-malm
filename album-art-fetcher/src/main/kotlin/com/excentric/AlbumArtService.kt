2941bd77032f0ad5b7a1fb8f7dc00bd032ad010b
scoughlan <sean.coughlan@utmost.co>
2025-05-31 18:28:30 +0100

    spring shell

---
+++ album-art-fetcher/src/main/kotlin/com/excentric/AlbumArtService.kt
@@ -0,0 +1,31 @@
+package com.excentric
+
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
+import org.springframework.shell.standard.ShellOption
+import org.springframework.stereotype.Service
+
+@Service
+class AlbumArtService(
+    private val metadataFetcher: MusicBrainzMetadataFetcher,
+    private val artDownloader: MusicBrainzAlbumArtDownloader
+) {
+    fun findAndDownloadAlbumArt(artist: String, album: String, outputPath: String): String {
+        val mbid = metadataFetcher.findMbidByAlbumInfo(artist, album)
+        artDownloader.downloadAlbumArt(mbid, outputPath)
+        return "Album art for '$album' by '$artist' downloaded to $outputPath"
+    }
+}
+
+@ShellComponent
+class AlbumArtCommands(private val albumArtService: AlbumArtService) {
+    
+    @ShellMethod(key = ["find-and-download"], value = "Find and download album art in one step")
+    fun findAndDownload(
+        @ShellOption(help = "Artist name") artist: String,
+        @ShellOption(help = "Album name") album: String,
+        @ShellOption(help = "Output file path", defaultValue = "album_cover.jpg") outputPath: String
+    ): String {
+        return albumArtService.findAndDownloadAlbumArt(artist, album, outputPath)
+    }
+}
