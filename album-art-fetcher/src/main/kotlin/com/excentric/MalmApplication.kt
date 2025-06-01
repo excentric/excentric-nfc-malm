3ebc1d1dc15a4cdeca078b4271dde54bb2ec1a1e
scoughlan <sean.coughlan@utmost.co>
2025-06-01 12:31:24 +0100

    bad checkpoint

--- album-art-fetcher/src/main/kotlin/com/excentric/MalmApplication.kt
+++ album-art-fetcher/src/main/kotlin/com/excentric/MalmApplication.kt
@@ -2,20 +2,26 @@ package com.excentric
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.jline.utils.AttributedString
+import org.jline.utils.AttributedStyle
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.shell.command.annotation.CommandScan
+import org.springframework.shell.jline.PromptProvider
 import org.springframework.web.client.RestTemplate
 import java.time.Duration.ofSeconds
+
 
 fun main(args: Array<String>) {
     SpringApplication.run(MusicBrainzApplication::class.java, *args)
 }
 
 @SpringBootApplication
+@CommandScan
 @ConfigurationPropertiesScan("com.excentric.config")
 open class MusicBrainzApplication {
     companion object {
@@ -30,6 +36,11 @@ open class MusicBrainzApplication {
             .build().apply {
                 messageConverters.add(0, MappingJackson2HttpMessageConverter())
             }
+    }
+
+    @Bean
+    open fun myPromptProvider(): PromptProvider {
+        return PromptProvider { AttributedString("my-shell:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW)) }
     }
 
     @Bean
