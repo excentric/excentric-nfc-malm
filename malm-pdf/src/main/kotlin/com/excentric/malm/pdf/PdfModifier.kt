f5aba4fd8d4ceafb12c4ce0c6aec8385cd9fa0c7
scoughlan <sean.coughlan@utmost.co>
2025-06-02 12:18:55 +0100

    aligned pdf (slot 1)

--- malm-pdf/src/main/kotlin/com/excentric/malm/pdf/PdfModifier.kt
+++ malm-pdf/src/main/kotlin/com/excentric/malm/pdf/PdfModifier.kt
@@ -5,10 +5,12 @@ import com.itextpdf.kernel.pdf.PdfReader
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.layout.Document
 import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Image
 import com.itextpdf.kernel.font.PdfFontFactory
 import com.itextpdf.io.font.constants.StandardFonts
 import com.itextpdf.layout.properties.TextAlignment
 import com.itextpdf.layout.borders.SolidBorder
+import com.itextpdf.io.image.ImageDataFactory
 import java.io.File
 import java.io.FileOutputStream
 
@@ -51,7 +53,7 @@ class PdfModifier {
             // Create Sans-serif font with 62px size
             // Using Helvetica as it's a standard sans-serif font
             val font = PdfFontFactory.createFont(StandardFonts.HELVETICA)
-            val fontSize = 22f
+            val fontSize = 17f
 
             // Create paragraph with the text
             val paragraph = Paragraph("Pink Floyd\nAnimals\n(1971)")
@@ -64,15 +66,41 @@ class PdfModifier {
             paragraph.setTextAlignment(TextAlignment.CENTER)
 
             // Add a 1px border around the paragraph
-            paragraph.setBorder(SolidBorder(1f))
+//            paragraph.setBorder(SolidBorder(1f))
 
             // Set to vertical orientation by rotating 90 degrees
             paragraph.setRotationAngle(Math.PI / 2)
 
             // Position the text
-            paragraph.setFixedPosition(272f, 680f, 120f) // x, y, width (increased width for vertical text)
+            paragraph.setFixedPosition(275f, 675f, 130f) // x, y, width (increased width for vertical text)
 
             document.add(paragraph)
+
+            // Load the image from the specified path
+            val imagePath = System.getProperty("user.home") + "/.album-metadata/1/1.jpg"
+            val imageFile = File(imagePath)
+
+            if (imageFile.exists()) {
+                // Create an image object
+                val image = Image(ImageDataFactory.create(imagePath))
+
+                // Resize the image to 300x300
+                image.setWidth(130f)
+                image.setHeight(130f)
+
+                // Rotate the image (90 degrees = PI/2 radians)
+                image.setRotationAngle(Math.PI / 2)
+
+                // Position the image at (20, 20)
+                image.setFixedPosition(58f, 670f)
+
+                // Add the image to the document
+                document.add(image)
+
+                println("Image added from: $imagePath")
+            } else {
+                println("Warning: Image file not found at: $imagePath")
+            }
 
             // Close the document
             document.close()
