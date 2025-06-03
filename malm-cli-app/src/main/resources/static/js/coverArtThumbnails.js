a0d9e1ffa0c254327224b8afb9d7908df0ceaec1
scoughlan <sean.coughlan@utmost.co>
2025-06-03 02:34:46 +0100

    cover art popup js

---
+++ malm-cli-app/src/main/resources/static/js/coverArtThumbnails.js
@@ -0,0 +1,28 @@
+// Get the modal
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("fullImage");
+
+// Function to open the modal
+function openModal(imgSrc) {
+    modal.style.display = "block";
+    modalImg.src = imgSrc;
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Close the modal when clicking outside the image
+window.onclick = function (event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+}
+
+// Close the modal when ESC key is pressed
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
